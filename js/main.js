e999cb50b3178ad141a13d006fe5c9553606bbb7
Vanessa Santana <vanessah4money@gmail.com>
2022-03-29 22:33:47 -0300

    feat: finalizando menu mobile

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@ let menu_hambuguer = document.querySelector(".header__menu-mobile");
 let menu = document.querySelector(".header__menu-desk");
 
 function abrirMenu () {
-  menu.classList.toggle("mobile-ativo");
+  let ativo = menu.classList.toggle("mobile-ativo");
+  let img = menu_hambuguer.querySelector("img");
+
+  if (ativo) {
+    img.setAttribute("src", "images/icon-close.svg");
+    return;
+  }
+
+  img.setAttribute("src", "images/icon-hamburger.svg");
 };
 menu_hambuguer.addEventListener("click", abrirMenu);
 
